d17f61515afc6f59e43cc9395d0fcc137f9e8a45
A-leks-andr <calllamurzakov@gmail.com>
2025-06-04 17:19:31 +0300

    fix gendiff.scripts.gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,7 +1,10 @@
 from gendiff.gendiff_help import parse
+from gendiff.data import read_data
 
 def main():
-	parse()
+	args = parse()
+	print(read_data(args.first_file))
+	print(read_data(args.second_file))
 
 
 if __name__=='__main__':
