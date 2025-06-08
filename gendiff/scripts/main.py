b3abaa4ded633b243ef9f65e9a68ee45073d0b4e
A-leks-andr <calllamurzakov@gmail.com>
2025-06-08 04:58:41 +0300

    fix main.py

--- gendiff/scripts/main.py
+++ gendiff/scripts/main.py
@@ -3,11 +3,9 @@ from gendiff.generate import generate_diff
 
 
 def main():
-	args = parse()
-	print(
-		generate_diff(args.first_file, args.second_file)
-		)
-	
-	
-if __name__ == '__main__':
-	main()
+    args = parse()
+    print(generate_diff(args.first_file, args.second_file))
+
+
+if __name__ == "__main__":
+    main()
