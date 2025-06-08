13130789cd4c6fbb9be795eeae0c49dc64b1e73e
A-leks-andr <calllamurzakov@gmail.com>
2025-06-08 18:05:47 +0300

    fix main.py

--- gendiff/scripts/main.py
+++ gendiff/scripts/main.py
@@ -1,10 +1,13 @@
+import json
+
 from gendiff.gendiff_help import parse
 from gendiff.generate import generate_diff
 
 
 def main():
     args = parse()
-    print(generate_diff(args.first_file, args.second_file))
+    result = generate_diff(args.first_file, args.second_file)
+    print(json.dumps(result, indent=4))
 
 
 if __name__ == "__main__":
