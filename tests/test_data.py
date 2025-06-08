972e475b2b3f00feb068398fa261b95d836e3094
A-leks-andr <calllamurzakov@gmail.com>
2025-06-08 14:56:13 +0300

    fix test_data.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,21 +1,24 @@
+import json
 from pathlib import Path
 
 from gendiff.data import read_data
 
 
 def get_test_data_path(filename):
-    return Path(__file__).parent / "tests" / filename
+    return Path(__file__).parent / filename
 
 
 def read_file(filename):
-    return get_test_data_path(filename).read_text()
+    with open(get_test_data_path(filename), "r") as file:
+        text = json.load(file)
+    return text
 
 
 def test_read_data():
     file1 = get_test_data_path("file_1.json")
     file2 = get_test_data_path("file_2.json")
-    expec1 = read_file("r_data1.txt")
-    expec2 = read_file("r_data2.txt")
+    expec1 = read_file("file_1.json")
+    expec2 = read_file("file_2.json")
 
     assert read_data(file1) == expec1
     assert read_data(file2) == expec2
