5066e7a1936dce0a62203d2b3e457eebd1cca50c
A-leks-andr <calllamurzakov@gmail.com>
2025-06-08 04:58:05 +0300

    fix search_of_the_diff.py

--- gendiff/search_of_the_diff.py
+++ gendiff/search_of_the_diff.py
@@ -2,17 +2,17 @@ import json
 
 
 def search_diff(value1, value2):
-	result = {}
-	all_keys = sorted(list(value1.keys() | value2.keys()))
-	for key in all_keys:
-		if key in value1 and key not in value2:
-			result[f'- {key}'] = value1.get(key)
-		elif key in value1 and key in value2:
-			if value1[key] == value2[key]:
-				result[f'  {key}'] = value1.get(key)
-			else:
-				result[f'- {key}'] = value1.get(key)
-				result[f'+ {key}'] = value2.get(key)
-		elif key not in value1 and key in value2:
-			result[f'+ {key}'] = value2.get(key)
-	return json.dumps(result, indent=4)
+    result = {}
+    all_keys = sorted(list(value1.keys() | value2.keys()))
+    for key in all_keys:
+        if key in value1 and key not in value2:
+            result[f"- {key}"] = value1.get(key)
+        elif key in value1 and key in value2:
+            if value1[key] == value2[key]:
+                result[f"  {key}"] = value1.get(key)
+            else:
+                result[f"- {key}"] = value1.get(key)
+                result[f"+ {key}"] = value2.get(key)
+        elif key not in value1 and key in value2:
+            result[f"+ {key}"] = value2.get(key)
+    return json.dumps(result, indent=4)
