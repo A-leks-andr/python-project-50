be9c9d6c1de204da9d1dad4afdf326d760bb54e3
A-leks-andr <calllamurzakov@gmail.com>
2025-06-08 04:57:13 +0300

    fix generate.py

--- gendiff/generate.py
+++ gendiff/generate.py
@@ -3,6 +3,6 @@ from gendiff.search_of_the_diff import search_diff
 
 
 def generate_diff(first_file, second_file):
-	data_1 = read_data(first_file)
-	data_2 = read_data(second_file)
-	return search_diff(data_1, data_2)
+    data_1 = read_data(first_file)
+    data_2 = read_data(second_file)
+    return search_diff(data_1, data_2)
