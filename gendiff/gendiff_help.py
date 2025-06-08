03388e5174bd7269aa6d2adf0209d3f34f188e5a
A-leks-andr <calllamurzakov@gmail.com>
2025-06-08 04:56:22 +0300

    fix gendiff_help.py

--- gendiff/gendiff_help.py
+++ gendiff/gendiff_help.py
@@ -2,14 +2,11 @@ import argparse
 
 
 def parse():
-	parser = argparse.ArgumentParser(
-		description='Compares two configuration ' 
-		'files and shows a difference.'
-		)
-	
-	parser.add_argument('first_file')
-	parser.add_argument('second_file')
-	parser.add_argument(
-		'-f', '--format', help='set forman of output'
-		)
-	return parser.parse_args()
+    parser = argparse.ArgumentParser(
+        description="Compares two configuration files and shows a difference."
+    )
+
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
+    parser.add_argument("-f", "--format", help="set forman of output")
+    return parser.parse_args()
