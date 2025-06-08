e7104ea46250c955ecfbcb2cbffb775b2c5c60a1
A-leks-andr <calllamurzakov@gmail.com>
2025-06-08 05:00:02 +0300

    add test_data.py

---
+++ tests/test_data.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+from gendiff.data import read_data
+
+
+def get_test_data_path(filename):
+    return Path(__file__).parent / "test_data" / filename
+
+
+def read_file(filename):
+    return get_test_data_path(filename).read_text()
+
+
+def test_read_data():
+    file1 = get_test_data_path("file_1.json")
+    file2 = get_test_data_path("file_2.json")
+    expec1 = read_file("r_data1.txt")
+    expec2 = read_file("r_data2.txt")
+
+    assert read_data(file1) == expec1
+    assert read_data(file2) == expec2
