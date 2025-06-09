5c2e9fe859124f3513d9d93b106fd5d8f7e684a6
A-leks-andr <calllamurzakov@gmail.com>
2025-06-09 22:00:13 +0300

    fix test_read.py

--- tests/test_read.py
+++ tests/test_read.py
@@ -1,6 +1,8 @@
 import json
 from pathlib import Path
 
+import yaml
+
 from gendiff.data import read_data
 
 
@@ -9,14 +11,28 @@ def get_test_data_path(filename):
 
 
 def read_file(filename):
-    with open(get_test_data_path(filename), "r") as file:
-        text = json.load(file)
+    if Path(filename).suffix == ".json":
+        with open(get_test_data_path(filename), "r") as file:
+            text = json.load(file)
+    elif Path(filename).suffix in (".yaml", "yml"):
+        with open(filename, "r") as file:
+            text = yaml.load(file, Loader=yaml.SafeLoader)
     return text
 
 
-def test_read_data():
+def test_read_data_json():
     file1 = get_test_data_path("file_1.json")
     file2 = get_test_data_path("file_2.json")
+    expec1 = read_file("file_1.json")
+    expec2 = read_file("file_2.json")
+
+    assert read_data(file1) == expec1
+    assert read_data(file2) == expec2
+
+
+def test_read_data_yaml():
+    file1 = get_test_data_path("file_1.yaml")
+    file2 = get_test_data_path("file_2.yaml")
     expec1 = read_file("file_1.json")
     expec2 = read_file("file_2.json")
 
