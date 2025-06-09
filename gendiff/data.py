bc8fc47b9c803db289fa119bf1cc713fc122377d
A-leks-andr <calllamurzakov@gmail.com>
2025-06-09 22:01:10 +0300

    fix data.py

--- gendiff/data.py
+++ gendiff/data.py
@@ -1,7 +1,14 @@
 import json
+from pathlib import Path
+
+import yaml
 
 
 def read_data(path_file):
-    with open(path_file, "r") as file:
-        strings = json.load(file)
+    if Path(path_file).suffix == ".json":
+        with open(path_file, "r") as file:
+            strings = json.load(file)
+    elif Path(path_file).suffix in (".yaml", ".yml"):
+        with open(path_file, "r") as file:
+            strings = yaml.load(file, Loader=yaml.SafeLoader)
     return strings
