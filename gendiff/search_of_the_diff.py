f30986daa546b87003cf195968b9cdc8b362dc5e
A-leks-andr <calllamurzakov@gmail.com>
2025-06-12 02:29:50 +0300

    fix search_of_the_diff.py

--- gendiff/search_of_the_diff.py
+++ gendiff/search_of_the_diff.py
@@ -14,7 +14,8 @@ def search_diff(value1, value2):  # noqa: C901
                 ):
                     diff_dict[f"  {k}"] = value1[k]
                 elif value1[k] == value2[k] and (
-                    isinstance(value1[k], dict) or isinstance(value2, dict)
+                    isinstance(value1[k], dict) 
+                    or isinstance(value2, dict)
                 ):
                     if f"  {k}" not in diff_dict:
                         diff_dict[f"  {k}"] = {}
@@ -26,11 +27,26 @@ def search_diff(value1, value2):  # noqa: C901
                     diff_dict[f"- {k}"] = value1[k]
                     diff_dict[f"+ {k}"] = value2[k]
                 elif value1[k] != value2[k] and (
-                    isinstance(value1[k], dict) or isinstance(value2, dict)
+                    isinstance(value1[k], dict) 
+                    and isinstance(value2[k], dict)
                 ):
-                    if f"- {k}" not in diff_dict:
+                    if f"  {k}" not in diff_dict:
                         diff_dict[f"  {k}"] = {}
                     diff_dict[f"  {k}"].update(inner(value1[k], value2[k]))
+                elif value1[k] != value2[k] and (
+                    isinstance(value1[k], dict) 
+                    and not isinstance(value2[k], dict)):
+                    if f"- {k}" not in diff_dict:
+                        diff_dict[f"- {k}"] = {}
+                    diff_dict[f"- {k}"].update(inner(value1[k], None))
+                    diff_dict[f'+ {k}'] = value2[k]
+                elif value1[k] != value2[k] and (
+                    not isinstance(value1[k], dict) 
+                    and isinstance(value2[k], dict)):
+                    diff_dict[f"- {k}"] = value1[k]
+                    if f"+ {k}" not in diff_dict:
+                        diff_dict[f"+ {k}"] = {}
+                    diff_dict[f"+ {k}"].update(inner(None, value1[k]))
             elif k in value1 and k not in value2:
                 if isinstance(value1[k], dict):
                     diff_dict[f"  {k}"] = inner(value1[k], None)
@@ -38,9 +54,8 @@ def search_diff(value1, value2):  # noqa: C901
                     diff_dict[f"- {k}"] = value1[k]
             elif k not in value1 and k in value2:
                 if isinstance(value2[k], dict):
-                    diff_dict[f"+ {k}"] = inner(None, value2[k])
+                    diff_dict[f"  {k}"] = inner(None, value2[k])
                 else:
                     diff_dict[f"+ {k}"] = value2[k]
         return diff_dict
-
     return inner(value1, value2)
